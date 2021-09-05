49c919d64cd15cf391440b6ea516af84357d7918
Andrzej Zabost <andrzej.zabost@brightinventions.pl>
2021-09-05 22:29:43 +0200

    Update to Kotlin 1.5.21

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
     const val group = "pl.brightinventions.kequality"
-    const val version = "1.2"
+    const val version = "1.3.0"
     const val jvmTarget = "1.6"
 }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val kotlin = "1.4.32"
+    const val kotlin = "1.5.21"
     const val shouldko = "0.2.2"
     const val recyclerView = "1.1.0"
     const val androidGradlePlugin = "3.6.2"
